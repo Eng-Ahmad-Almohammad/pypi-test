54644eaf7a7d6570caa4bbdd6a4d76da07cde785
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-07-28 17:08:14 +0300

    test pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='example_publish_pypi_devterminal',
-    version='1.0',
+    version='1.1',
     license='MIT',
     author="Ahmad Almohammad",
     author_email='almohammedahmed23@gmail.com',
